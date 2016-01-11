554e7bd58e8c63120d63cf9461a920df097e0fe1
Saher El-Neklawy <saher.neklawy@gmail.com>
2016-01-11 16:25:00 +0200

    Finalizing ls functionality

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ function getConfigFilePath() {
   return path.join(getUserHome(), config_file_name)
 }
 
+function parseUri(uri) {
+  var protocol_split = (uri || "").split("://");
+  if(protocol_split.length==1)
+    protocol_split.unshift(null)
+  var protocol = protocol_split[0];
+  var path = protocol_split[1];
+  var is_local = !protocol || protocol == 'file';
+  var account;
+  var container;
+  if(protocol == 'blob') {
+    var path_split = path.split('/');
+    account = path_split.shift();
+    container = path_split.shift();
+    path = path_split.join('/');
+  }
+  return {
+    is_local: is_local,
+    protocol: protocol,
+    path: path,
+    account: account,
+    container: container
+  }
+}
+
 function getUserHome() {
   return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
@@ -33,6 +57,9 @@ function getBlobService(key) {
   if(!key) {
     throw new Error("Need to at least have 1 account added first");
   }
+  if(!config.accounts[key]) {
+    throw new Error("Account '" + key + "' not found!");
+  }
   var name = config.accounts[key].name;
   var key = config.accounts[key].key;
   return azure.createBlobService(name, key);
@@ -45,8 +72,14 @@ function print_list(l) {
   console.log(l.join("\n"))
 }
 
-function ls(callback, container) {
-  var blobService = getBlobService();
+function build_azure_blob_name(account, container, name) {
+  var l = ["blob:/", account, container, name];
+  l = _.filter(l, function(i) {return !!i});
+  return l.join("/");
+}
+
+function ls_account(callback, account, container) {
+  var blobService = getBlobService(account);
   if(container) {
     blobService.listBlobsSegmented(container, currentToken,
         function(error, result, response) {
@@ -57,7 +90,7 @@ function ls(callback, container) {
             var size = i.properties['content-length']/1024.0/1024.0;
             return [size.toPrecision(2) + "MB",
                     //i.properties['last-modified'],
-                    i.name].join("\t");
+                    build_azure_blob_name(account, container, i.name)].join("\t");
           }));
         });
   }
@@ -66,8 +99,27 @@ function ls(callback, container) {
       currentToken = result.continuationToken;
       if(program.verbose)
         console.log(JSON.stringify(result, null, 2));
-      callback(_.map(result.entries, function(i){return i.name}));
+      callback(_.map(result.entries, function(i){return build_azure_blob_name(account, container, i.name)}));
     })
+  }
+}
+
+function ls(callback, uri) {
+  var uri_obj = parseUri(uri);
+  if(program.verbose)
+    console.log(JSON.stringify(uri_obj, null, 2));
+  if(!uri || !uri_obj.is_local) {
+    if(!uri_obj.account) {
+      _.mapKeys(read_config().accounts, function(value, account_key) {
+        ls_account(callback, account_key);
+      })
+    }
+    else {
+        ls_account(callback, uri_obj.account, uri_obj.container);
+    }
+  }
+  else {
+    throw Error("Please specify a valid blob URI")
   }
 }
 
@@ -105,7 +157,7 @@ program
 .option('-v, --verbose', 'Run in verbose mode')
 
 program
-  .command('ls [container]')
+  .command('ls [uri]')
   .action(function(container) {ls(print_list, container)});
 
 program
