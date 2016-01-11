0aa73756958bf3d47169315f663ebdf70203e672
Saher El-Neklawy <saher.neklawy@gmail.com>
2016-01-11 12:52:53 +0200

    Adding ls inside container

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ var azure = require('azure-storage');
 var our_account_name='kagglesaher'
 var our_account_key='paBrkQ6r2rZW7+lLixmdG6UJwbFy86pKS8CnF8WRCs+lHYANyLjPIqqvl1XYs1kd4ffHh1aXle+ypqhu4ym2IQ=='
 var our_container = "uploadedresources"
+var blobService = azure.createBlobService(our_account_name, our_account_key);
+var currentToken = undefined;
+
+function print_list(l) {
+  console.log(l.join("\n"))
+}
 
 function ls(callback, container) {
-  var blobService = azure.createBlobService(our_account_name, our_account_key);
-  var currentToken = undefined;
-  blobService.listContainersSegmented(currentToken, function(error, result, response) {
-    currentToken = result.continuationToken;
-    if(program.verbose)
-      console.log(JSON.stringify(result, null, 2));
-    callback(_.map(result.entries, function(i){return i.name}));
-  })
+  if(container) {
+    blobService.listBlobsSegmented(container, currentToken,
+        function(error, result, response) {
+          currentToken = result.continuationToken;
+          if(program.verbose)
+            console.log(JSON.stringify(result, null, 2));
+          callback(_.map(result.entries, function(i){
+            var size = i.properties['content-length']/1024.0/1024.0;
+            return [size.toPrecision(2) + "MB",
+                    //i.properties['last-modified'],
+                    i.name].join("\t");
+          }));
+        });
+  }
+  else {
+    blobService.listContainersSegmented(currentToken, function(error, result, response) {
+      currentToken = result.continuationToken;
+      if(program.verbose)
+        console.log(JSON.stringify(result, null, 2));
+      callback(_.map(result.entries, function(i){return i.name}));
+    })
+  }
 }
 
 function configure() {
@@ -28,7 +48,7 @@ program
 
 program
   .command('ls [container]')
-  .action(function(container) {ls(console.log, container)});
+  .action(function(container) {ls(print_list, container)});
 
 program
   .command('configure')
